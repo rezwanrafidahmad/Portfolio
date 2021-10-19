177f13d8960fa1f5a1c5f9071273aac20b7a6116
rezwanrafidahmad <92386553+rezwanrafidahmad@users.noreply.github.com>
2021-10-20 01:57:52 +0600

    training_1

--- src/Documentation/Training/data.js
+++ src/Documentation/Training/data.js
@@ -1,25 +1,34 @@
 import csm from 'img/csm.png'
+import capm_logo from 'img/capm_logo.png'
 
 const data = [
   {
     id: 1,
     img: csm,
-    title: 'Certified Associate in Project Management (CAPM) Training',
-    date: 'Jan 2021 - Apr 2021',
-    institute: 'Day Spring Ltd.',
-    description: 'A PMI® Approved Premium Training Partner (ATP)',
-
-    skills: ['Project Management', 'Agile Practices', 'Risk Assessment'],
+    title: 'Certified Scrum Master (CSM)',
+    date: 'June 2021',
+    institute: 'ScrumAlliance',
+    description: '16-hour Instructor-led online training on fundamentals of Scrum and Agile methodology',
+    skills: ['Scrum', 'Agile Methodology', 'Software Development'],
     link: 'https://drive.google.com/file/d/1F2ikWaC-qbiLapdgPRmb3vH1i78HWd53/view?usp=sharing',
   },
   {
     id: 2,
+    img: capm_logo,
+    title: 'Certified Associate in Project Management (CAPM) Training',
+    date: 'March 2021',
+    institute: 'Day Spring Ltd. - A PMI® Approved Premium Training Partner (ATP)',
+    description: '3 month long training to prepare for the Certified Associate in Project Management (CAPM) exam based on PMBOK 6th ed. Earned 35 Professional Development Units (PDU)',
+    skills: ['Project Management', 'Agile Practices', 'Risk Assessment'],
+    link: 'https://drive.google.com/file/d/1F2ikWaC-qbiLapdgPRmb3vH1i78HWd53/view?usp=sharing',
+  },
+  {
+    id: 3,
     img: csm,
     title: 'Certified Associate in Project Management (CAPM) Training',
     date: 'Jan 2021 - Apr 2021',
     institute: 'Day Spring Ltd.',
     description: 'A PMI® Approved Premium Training Partner (ATP)',
-
     skills: ['Project Management', 'Agile Practices', 'Risk Assessment'],
     link: 'https://drive.google.com/file/d/1F2ikWaC-qbiLapdgPRmb3vH1i78HWd53/view?usp=sharing',
   },
