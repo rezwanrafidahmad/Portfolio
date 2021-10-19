ff98485e8fd394f0781607a071e14f8089873f18
rezwanrafidahmad <92386553+rezwanrafidahmad@users.noreply.github.com>
2021-10-20 03:02:24 +0600

    training_2

--- src/Documentation/Training/data.js
+++ src/Documentation/Training/data.js
@@ -10,7 +10,7 @@ const data = [
     institute: 'ScrumAlliance',
     description: '16-hour Instructor-led online training on fundamentals of Scrum and Agile methodology',
     skills: ['Scrum', 'Agile Methodology', 'Software Development'],
-    link: 'https://drive.google.com/file/d/1F2ikWaC-qbiLapdgPRmb3vH1i78HWd53/view?usp=sharing',
+    link: 'https://drive.google.com/file/d/1-fl-l9zcanwkObOwaZIOIZydCeveBkED/view?usp=sharing',
   },
   {
     id: 2,
@@ -20,17 +20,17 @@ const data = [
     institute: 'Day Spring Ltd. - A PMI® Approved Premium Training Partner (ATP)',
     description: '3 month long training to prepare for the Certified Associate in Project Management (CAPM) exam based on PMBOK 6th ed. Earned 35 Professional Development Units (PDU)',
     skills: ['Project Management', 'Agile Practices', 'Risk Assessment'],
-    link: 'https://drive.google.com/file/d/1F2ikWaC-qbiLapdgPRmb3vH1i78HWd53/view?usp=sharing',
+    link: 'https://drive.google.com/file/d/14itAPgPluBRz8M75RVgepvTduQwRGvmj/view?usp=sharing',
   },
   {
     id: 3,
     img: csm,
-    title: 'Certified Associate in Project Management (CAPM) Training',
-    date: 'Jan 2021 - Apr 2021',
-    institute: 'Day Spring Ltd.',
-    description: 'A PMI® Approved Premium Training Partner (ATP)',
-    skills: ['Project Management', 'Agile Practices', 'Risk Assessment'],
-    link: 'https://drive.google.com/file/d/1F2ikWaC-qbiLapdgPRmb3vH1i78HWd53/view?usp=sharing',
+    title: 'Top-up IT (Java)',
+    date: 'March 2018',
+    institute: 'Leveraging ICT for Growth, Employment and Governance (LICT) - Bangladesh Computer Council',
+    description: '260-hour Instructor led live training on Fundamentals ~ Advanced Java for Enterprise Application Development.',
+    skills: ['Java SE', 'Java EE', 'Spring MVC', 'Spring Boot', 'Hibernate', 'MySQL', 'HTML/CSS'],
+    link: 'https://drive.google.com/file/d/1NV8UNWuU07wZ8DccihdcLD8Txl5YX4ez/view?usp=sharing',
   },
 ]
 
