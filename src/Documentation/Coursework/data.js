5d2ce6ffd776577c3fbfa8ca78506fe645eb3d5d
rezwanrafidahmad <92386553+rezwanrafidahmad@users.noreply.github.com>
2021-10-19 21:15:55 +0600

    coursework_1

--- src/Documentation/Coursework/data.js
+++ src/Documentation/Coursework/data.js
@@ -1,12 +1,13 @@
 import pcap from 'img/pcap.png'
+import ibm_logo from 'img/ibm_logo.png'
 
 const data = [
   {
     id: 1,
     img: pcap,
-    title: 'Certified Associate in Project Management (CAPM) Training',
-    date: 'Jan 2021 - Apr 2021',
-    institute: 'Day Spring Ltd.',
+    title: 'Deep Learning Specialization',
+    date: 'Oct 2021',
+    institute: 'Deeplearning.ai (Coursera)',
     description: 'A PMI® Approved Premium Training Partner (ATP)',
 
     skills: ['Project Management', 'Agile Practices', 'Risk Assessment'],
@@ -14,14 +15,13 @@ const data = [
   },
   {
     id: 2,
-    img: pcap,
-    title: 'Certified Associate in Project Management (CAPM) Training',
-    date: 'Jan 2021 - Apr 2021',
-    institute: 'Day Spring Ltd.',
+    img: ibm_logo,
+    title: 'IBM Professional Certificate in Data Science',
+    date: 'March 2020',
+    institute: 'IBM (Coursera)',
     description: 'A PMI® Approved Premium Training Partner (ATP)',
-
-    skills: ['Project Management', 'Agile Practices', 'Risk Assessment'],
-    link: 'https://drive.google.com/file/d/1F2ikWaC-qbiLapdgPRmb3vH1i78HWd53/view?usp=sharing',
+    skills: ['Python', 'Databases and SQL', 'Machine Learning', 'Data Visualization', 'Data Analysis'],
+    link: 'https://drive.google.com/file/d/1u66Z0wYocHvc2-zSM1B38kinYDBZwFm-/view?usp=sharing',
   },
 ]
 
