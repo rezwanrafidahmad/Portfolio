f8bca1a0b400b7e0bf3a3fa2318cbcc8500e1d11
rezwanrafidahmad <92386553+rezwanrafidahmad@users.noreply.github.com>
2021-10-21 00:24:46 +0600

    project_fix_3

--- src/Documentation/Portfilo/data.js
+++ src/Documentation/Portfilo/data.js
@@ -1,15 +1,10 @@
 import pro1 from 'img/pro1.jpg'
-import pro2 from 'img/pro2.jpg'
-import pro3 from 'img/pro3.jpg'
-import pro4 from 'img/pro4.jpg'
-import pro5 from 'img/pro5.jpg'
-import pro6 from 'img/pro6.jpg'
 
 const data = [
   {
     id: 1,
     name: 'Project1',
-    
+
     img1: pro1,
     img2: pro1,
 
