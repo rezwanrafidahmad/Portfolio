79abbfbff46f6c5bd9e83c24f482ccf64c4ae31d
rezwanrafidahmad <92386553+rezwanrafidahmad@users.noreply.github.com>
2021-10-24 22:31:51 +0600

    coursework_add

--- src/Documentation/Coursework/data.js
+++ src/Documentation/Coursework/data.js
@@ -5,6 +5,16 @@ const data = [
   {
     id: 1,
     img: deep_learning_1,
+    title: 'Natural Language Processing (NLP)',
+    date: 'Oct 2021 - Present',
+    institute: 'Deeplearning.ai',
+    description: 'A 4-course specialization offered by deeplearning.ai focusing on NLP',
+    skills: ['Neural Networks', 'Sentiment Analysis', 'Hidden Markov Model', 'Vector Space Models','Attention Models','Transformers','Machine Translation','Siamese Networks'],
+    link: 'https://www.coursera.org/specializations/natural-language-processing',
+  },
+  {
+    id: 2,
+    img: deep_learning_1,
     title: 'Deep Learning Specialization',
     date: 'Oct 2021',
     institute: 'Deeplearning.ai',
@@ -13,7 +23,7 @@ const data = [
     link: 'https://drive.google.com/file/d/1JIeMTh9fgF0nT0_l1I4WtU6Kwl34QozM/view?usp=sharing',
   },
   {
-    id: 2,
+    id: 3,
     img: ibm_badge_1,
     title: 'IBM Professional Certificate on Data Science',
     date: 'April 2020',
