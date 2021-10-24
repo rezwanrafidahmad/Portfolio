92df5aa140aecaa08033c9573a1262b65ec9eba4
rezwanrafidahmad <92386553+rezwanrafidahmad@users.noreply.github.com>
2021-10-24 22:44:51 +0600

    publication_update_4

--- src/Documentation/Publications/data.js
+++ src/Documentation/Publications/data.js
@@ -19,7 +19,7 @@ const data = [
     date: 'Nov 2020',
     institute: '2020 IEEE Region 10 Symposium (TENSYMP)',
     description: 'Software Engineering and HCI Research Group, Department of Computer Science and Engineering, BRAC University',
-    skills: ['Digital Signal Processing (DSP)', 'EEG', 'Epilepsy', 'Support Vector Machine (SVM)'],
+    skills: ['Digital Signal Processing (DSP)', 'EEG', 'Epilepsy', 'Support Vector Machine (SVM)','Regularization'],
     link: 'https://ieeexplore.ieee.org/abstract/document/9230899',
   },
 ]
