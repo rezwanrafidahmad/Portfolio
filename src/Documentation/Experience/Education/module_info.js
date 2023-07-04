3fe0e024e86b833d2964d58216fd696ae1d7f1bf
rezwanrafidahmad <92386553+rezwanrafidahmad@users.noreply.github.com>
2023-07-04 21:45:42 +0100

    lklk

--- src/Documentation/Experience/Education/module_info.js
+++ src/Documentation/Experience/Education/module_info.js
@@ -1,6 +1,6 @@
-const bsc_modules = ['Python', 'R', 'Tensorflow', 'Apache Spark', 'Java', 'React']
+// const bsc_modules = ['Python', 'R', 'Tensorflow', 'Apache Spark', 'Java', 'React']
 
 const msc_modules = ['dsfdsg34', 'R', 'Tensorflow', 'Apache Spark', 'Java', 'React']
 
 
-export { bsc_modules, msc_modules }
+export { msc_modules }
