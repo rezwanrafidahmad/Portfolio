4108b7dde7419bfbad471c73954c6d03d8f8e060
rezwanrafidahmad <92386553+rezwanrafidahmad@users.noreply.github.com>
2023-07-05 14:08:41 +0100

    l

--- src/Documentation/Portfilo/data.js
+++ src/Documentation/Portfilo/data.js
@@ -1,7 +1,7 @@
 import monte_carlo_cover from 'img/monte_carlo_cover.jpg'
 import monte_carlo_1 from 'img/monte_carlo_1.jpg'
-import monte_carlo_2a from 'img/monte_carlo_2a.jpg'
-
+import monte_carlo_2 from 'img/monte_carlo_2.jpg'
+// import monte_carlo_3 from 'img/monte_carlo_3.jpg'
 
 const data = [
   {
@@ -10,11 +10,11 @@ const data = [
 
     img0: monte_carlo_cover,
     img1: monte_carlo_1,
-    img2: monte_carlo_2a,
-    img3: null,
+    img2: monte_carlo_2,
+    img3: 'img/monte_carlo_3.jpg',
 
-    imgSub1: '1. Warming up AWS Scalable Services - Lambda, EC2 and ',
-    imgSub2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum',
+    imgSub1: '1. Warming up AWS Scalable Services - AWS Lambda, EC2 and RDS',
+    imgSub2: '2. Calculating the risk and cost associated after warming up',
     imgSub3: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum',
     desc: 'Calculating risks with Monte Carlo Simulation on trading signals using a multi-cloud application across different cloud services and platforms. Scalable and elastic services such as AWS Lambda, EC2, RDS and Google App Engine were used to create an end-to-end SaaS infrastructure. Cost analysis between different services were performed and estimated operation costs were compared.',
     skills: ['HTML5/CSS3', 'Flask', 'Jinja2', 'AWS Lambda', 'AWS EC2', 'AWS RDS', 'Google App Engine'],
